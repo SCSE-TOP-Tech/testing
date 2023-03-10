f50854ca34d4ddb16faeb17b721a5c4d144c02c9
Bryan Tay <rivenbryan@gmail.com>
2023-03-10 13:57:25 +0800

    test: Title validation 1

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// testing by bryan
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Smith' })
   // test
